285027f423af3c02b5d310923eda03d0c85848f1
Siliprandi Diego <siliprandi.diego@osdbsesto>
2021-12-09 12:08:09 +0100

    Pablito

--- index.js
+++ index.js
@@ -1,8 +1,11 @@
+const { request } = require("express");
 var express = require("express");
 var apiServer = express();
+var fs = require("fs");
 
 var port = 3000;
 var host = "localhost";
+
 apiServer.listen(port, host, ()=>{
     console.log("server running at http://%s:%d", host, port);
 });
@@ -21,6 +24,37 @@ apiServer.get("/mioNome", (request, response)=>{
     response.send("Hi, your name is: "+request.query.name);
 });
 
+// http://localhost:3000/somma?a=34&b=35
 apiServer.get("/somma", (request, response)=>{
     response.send("Result = "+ (parseInt(request.query.a) + parseInt(request.query.b))); 
 });  //We can also use '- -' to do an addition without the parseInt
+
+// http://localhost:3000/student?id=1
+apiServer.get("/student", (request, response)=>{
+    console.log("student id: ", request.query.id);
+    //read file
+    fs.readFile("studenti.json", (err, data) =>{
+        if (err){
+            console.log("error: " + err);
+        } else {
+            var students = JSON.parse(data);
+            response.send(
+                students.find(x => x.id === request.query.id)
+            );
+        }
+    });
+});
+
+// http://localhost:3000/newStudent?surname=Amati&name=Mattia&id=3
+apiServer.get("/newStudent", (request, response)=>{
+    var dt = {'surname': request.query.surname, 'name': request.query.name, 'id': request.query.id};
+    fs.readFile("studenti.json", (err, data) =>{
+        if (err){
+            console.log("error: " + err);
+        } else {
+            var datajson = JSON.parse(data);
+            datajson.push(dt);
+            fs.writeFile('studenti.json', JSON.stringify(datajson), {flag: 'a'}, err => {});
+        }
+    });
+});
